aaf7d731874ddcd7b21b76d1b43d3cdecac8998f
RedBird96 <gt20181122@gmail.com>
2024-03-26 21:03:29 -0500

    Added log codes

--- node/cluster.go
+++ node/cluster.go
@@ -171,10 +171,12 @@ func (n *Node) disbandCluster(requestID string, replicas []peer.ID) error {
 	ctx, cancel := context.WithTimeout(context.Background(), consensusClusterSendTimeout)
 	defer cancel()
 
+	n.log.Info().Strs("peers", blockless.PeerIDsToStr(replicas)).Msg("before send disband cluster")
 	err := n.sendToMany(ctx, replicas, msgDisband)
 	if err != nil {
 		return fmt.Errorf("could not send cluster disband request (request: %s): %w", requestID, err)
 	}
+	n.log.Info().Strs("peers", blockless.PeerIDsToStr(replicas)).Msg("Sent to disband cluster")
 
 	n.log.Info().Err(err).Str("request", requestID).Strs("peers", blockless.PeerIDsToStr(replicas)).Msg("sent cluster disband request")
 
